1c5d395cc3371a366dfa626fcda19b2e7d9cbdd8
MARCANAKI <augustmarc16@gmail.com>
2025-09-10 22:07:28 +0100

    learnt varletconst

---
+++ VariablesDataTypes.js
@@ -0,0 +1,19 @@
+let name= 'chibuzor'
+let IsTall= false
+let HasBlueEyes= 'false'
+
+if (true) {
+  let age = 5;
+  console.log(age);
+  age = 10;
+  console.log(age);
+      const height = 185;
+      
+      console.log(height);
+  if (true) {
+    
+  }
+}
+
+
+console.log('age',)
